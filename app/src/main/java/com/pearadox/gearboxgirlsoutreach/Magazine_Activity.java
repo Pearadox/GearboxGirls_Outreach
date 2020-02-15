5fc940ab862c98ee82f3644b3fc9b2f0baa67034
Gale French <gale.french@gmail.com>
2020-02-15 14:39:29 -0600

    Add all new code from Sara - Magazine Issue #2

--- app/src/main/java/com/pearadox/gearboxgirlsoutreach/Magazine_Activity.java
+++ app/src/main/java/com/pearadox/gearboxgirlsoutreach/Magazine_Activity.java
@@ -1,53 +1,98 @@
 package com.pearadox.gearboxgirlsoutreach;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 public class Magazine_Activity extends AppCompatActivity {
     String TAG = "Magazine_Activity";        // This CLASS name
+   String num = " ";
+
     ImageView magPage;
     Button btn_Back, btn_Forward;
-    private int[] magImages = new int[] {
+    private int[] magIssue1 = new int[] {
             R.drawable.ssv1_coverpage, R.drawable.ssv1_p00, R.drawable.ssv1_p01, R.drawable.ssv1_p02,R.drawable.ssv1_p03,
             R.drawable.ssv1_p04, R.drawable.ssv1_p05, R.drawable.ssv1_p06, R.drawable.ssv1_p07, R.drawable.ssv1_p08,
             R.drawable.ssv1_p09, R.drawable.ssv1_p10, R.drawable.ssv1_p11, R.drawable.ssv1_p12,
     };
+
+    private int[] magIssue2 = new int[] {
+            R.drawable.ssv2_coverpage, R.drawable.ssv2_p00, R.drawable.ssv2_p01, R.drawable.ssv2_p02,R.drawable.ssv2_p03,
+            R.drawable.ssv2_p04, R.drawable.ssv2_p05, R.drawable.ssv2_p06, R.drawable.ssv2_p07, R.drawable.ssv2_p08,
+            R.drawable.ssv2_p09, R.drawable.ssv2_p10, R.drawable.ssv2_p11, R.drawable.ssv2_p12, R.drawable.ssv2_p13, R.drawable.ssv2_p14,
+    };
     String pg_2Display = "";
     int pg = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magazine);
         Log.i(TAG, "## Magazine_Activity  ##");
+        Bundle bundle = this.getIntent().getExtras();
+        num = (bundle.getString("iss"));
+        Log.d(TAG, "Issue =  " + num);
 
         final ImageView magPage = (ImageView) findViewById(R.id.magPage);
         btn_Back = (Button) findViewById(R.id.btn_Back);
         btn_Forward = (Button) findViewById(R.id.btn_Forward);
 
+//        final Spinner spinner_Issue = (Spinner) findViewById(R.id.spinner_Issue);
+//        ArrayAdapter adapter_Issue = new ArrayAdapter<String>(this, R.layout.issue_list_layout, issue);
+//        adapter_Issue.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner_Issue.setAdapter(adapter_Issue);
+//        spinner_Issue.setSelection(0, false);
+//        spinner_Issue.setOnItemSelectedListener(new Magazine_Activity.issue_listener());
+
+
         pg = 0;
-        pg_2Display = String.valueOf(magImages[pg]);
-        magPage.setImageResource(getResources().getIdentifier(pg_2Display, "drawable", getPackageName()));
         btn_Back.setEnabled(false);
+        switch (num) {
+            case "STEM Savvy V.1":
+                pg_2Display = String.valueOf(magIssue1[pg]);
+                magPage.setImageResource(getResources().getIdentifier(pg_2Display, "drawable", getPackageName()));
+                break;
+            case "STEM Savvy V.2":
+                pg_2Display = String.valueOf(magIssue2[pg]);
+                magPage.setImageResource(getResources().getIdentifier(pg_2Display, "drawable", getPackageName()));
+                break;
+            default:
+                pg_2Display = String.valueOf(magIssue1[pg]);
+                magPage.setImageResource(getResources().getIdentifier(pg_2Display, "drawable", getPackageName()));
+                break;
+        }
+
 
         // Start Listeners
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         btn_Back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d(TAG, "@@@  btn_Back  @@@  " + pg);
-                if (pg > 0) {
-                    pg --;
-                } else {
-                    btn_Back.setEnabled(false);
+                 if (pg > 0) {
+                     pg--;
+                 } else {
+                     btn_Back.setEnabled(false);
+                     btn_Forward.setEnabled(true);
+                 }
+                switch (num) {
+                    case "STEM Savvy V.1":
+                        pg_2Display = String.valueOf(magIssue1[pg]);
+                        break;
+                    case "STEM Savvy V.2":
+                        pg_2Display = String.valueOf(magIssue2[pg]);
+                        break;
+                    default:
+                        pg_2Display = String.valueOf(magIssue1[pg]);    // Default to Issue #1
+                        break;
                 }
-                pg_2Display = String.valueOf(magImages[pg]);
                 magPage.setImageResource(getResources().getIdentifier(pg_2Display, "drawable", getPackageName()));
             }
         });
@@ -55,18 +100,42 @@ public class Magazine_Activity extends AppCompatActivity {
         btn_Forward.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d(TAG, "@@@  btn_Forward  @@@  " + pg);
-                if (pg < magImages.length-1) {
-                    pg ++;
-                } else {
-                    btn_Forward.setEnabled(false);
+                switch (num) {
+                    case "STEM Savvy V.1":
+                        if (pg < magIssue1.length - 1) {
+                            pg++;
+                        } else {
+                            btn_Forward.setEnabled(false);
+                        }
+                        pg_2Display = String.valueOf(magIssue1[pg]);
+                        break;
+                    case "STEM Savvy V.2":
+                        if (pg < magIssue2.length - 1) {
+                            pg++;
+                        } else {
+                            btn_Forward.setEnabled(false);
+                        }
+                        pg_2Display = String.valueOf(magIssue2[pg]);
+                        break;
+                    default:
+                        if (pg < magIssue1.length - 1) {
+                            pg++;
+                        } else {
+                            btn_Forward.setEnabled(false);
+                        }
+                        pg_2Display = String.valueOf(magIssue1[pg]);    // Default to Issue #1
+                        break;
                 }
-                pg_2Display = String.valueOf(magImages[pg]);
                 magPage.setImageResource(getResources().getIdentifier(pg_2Display, "drawable", getPackageName()));
                 btn_Back.setEnabled(true);
             }
         });
+
     }
-
-
+    
 
 }
+
+
+
+
--- app/src/main/java/com/pearadox/gearboxgirlsoutreach/gg_Menu.java
+++ app/src/main/java/com/pearadox/gearboxgirlsoutreach/gg_Menu.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class gg_Menu extends AppCompatActivity {
     String TAG = "gg_Menu";        // This CLASS name
     Button btn_Tutorials, btn_Quizzes, btn_Magazine, btn_Games;
+    public static String[] issue  = new String []
+            {" ", "STEM Savvy V.1", "STEM Savvy V.2"};
 
+    String num = " ";
+    String issueName = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +30,24 @@ public class gg_Menu extends AppCompatActivity {
         btn_Magazine = (Button) findViewById(R.id.btn_Magazine);
         btn_Games = (Button) findViewById(R.id.btn_Games);
 
+        final Spinner spinner_Issue = (Spinner) findViewById(R.id.spinner_Issue);
+        ArrayAdapter adapter_Issue = new ArrayAdapter<String>(this, R.layout.issue_list_layout, issue);
+        adapter_Issue.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_Issue.setAdapter(adapter_Issue);
+        spinner_Issue.setSelection(0, false);
+        spinner_Issue.setOnItemSelectedListener(new issue_OnItemSelectedListner());
+
 
 // Start Listeners
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         btn_Magazine.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d(TAG, "@@@ btn_Magazine  @@@");
+                Log.d(TAG, "@@@ btn_Magazine  @@@ "  + issueName);
 
                 Intent mag_intent = new Intent(gg_Menu.this, Magazine_Activity.class);
+                Bundle issueNumber = new Bundle();
+                issueNumber.putString("iss",issueName);
+                mag_intent.putExtras(issueNumber);
                 startActivity(mag_intent);        // Start the Magazine activity
 
             }
@@ -38,6 +55,18 @@ public class gg_Menu extends AppCompatActivity {
 
     }
 
+    public class issue_OnItemSelectedListner implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+            Log.d(TAG, "####  Issues Listner  ####  " + pos);
+            num = adapterView.getItemAtPosition(pos).toString();
+            issueName = issue[pos];
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
 //###################################################################
 //###################################################################
 //###################################################################
